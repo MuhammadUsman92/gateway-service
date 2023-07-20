29f84066f371d424131e6f8cfd949149192e45b6
MuhammadUsman92 <106598317+MuhammadUsman92@users.noreply.github.com>
2023-07-20 11:23:32 +0500

    Update GlobalCorsConfig.java

--- src/main/java/com/muhammadusman92/gatewayservice/GlobalCorsConfig.java
+++ src/main/java/com/muhammadusman92/gatewayservice/GlobalCorsConfig.java
@@ -9,7 +9,23 @@ import reactor.core.publisher.Mono;
 import java.util.Collections; // Add this import
 import java.util.Arrays; // Add this import
 
-public class GlobalCorsConfig {
+@Configuration
+public class GlobalCorsConfig extends org.springframework.web.cors.CorsConfiguration {
 
-  
+  @Bean
+  public CorsWebFilter corsFilter() {
+    org.springframework.web.cors.CorsConfiguration corsConfiguration = new org.springframework.web.cors.CorsConfiguration();
+    corsConfiguration.setAllowCredentials(true);
+    corsConfiguration.addAllowedOrigin("*");
+    corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"));
+    corsConfiguration.addAllowedHeader("origin");
+    corsConfiguration.addAllowedHeader("content-type");
+    corsConfiguration.addAllowedHeader("accept");
+    corsConfiguration.addAllowedHeader("authorization");
+    corsConfiguration.addAllowedHeader("cookie");
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", corsConfiguration);
+    return new CorsWebFilter(source);
+  }
 }
+
