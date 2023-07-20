fff9b82909a929f46da822ad20bda3d264f1e888
MuhammadUsman92 <106598317+MuhammadUsman92@users.noreply.github.com>
2023-07-20 10:33:06 +0500

    Update GatewayServiceApplication.java

--- src/main/java/com/muhammadusman92/gatewayservice/GatewayServiceApplication.java
+++ src/main/java/com/muhammadusman92/gatewayservice/GatewayServiceApplication.java
@@ -26,6 +26,16 @@ public class GatewayServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
+	@Bean
+ public WebMvcConfigurer configure() {
+  return new WebMvcConfigurer() {
+   @Override
+   public void addCorsMappings(CorsRegistry reg) {
+    reg.addMapping("/**").allowedOrigins("*");
+   }
+  };
+  
+ }
 
 	@Bean
 	@LoadBalanced
