7bdae2185a02af8cf6c7231a0b84ebb002fc5d96
MuhammadUsman92 <106598317+MuhammadUsman92@users.noreply.github.com>
2023-07-20 10:14:18 +0500

    Update GlobalCorsConfig.java

--- src/main/java/com/muhammadusman92/gatewayservice/GlobalCorsConfig.java
+++ src/main/java/com/muhammadusman92/gatewayservice/GlobalCorsConfig.java
@@ -9,38 +9,7 @@ import reactor.core.publisher.Mono;
 import java.util.Collections; // Add this import
 import java.util.Arrays; // Add this import
 
-@Configuration
 public class GlobalCorsConfig {
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Collections.singletonList("*"));
-        configuration.setAllowedMethods(Arrays.asList(
-                HttpMethod.GET.name(),
-                HttpMethod.POST.name(),
-                HttpMethod.PUT.name(),
-                HttpMethod.DELETE.name(),
-                HttpMethod.OPTIONS.name()
-        ));
-        configuration.setAllowedHeaders(Collections.singletonList("Content-Type"));
-        configuration.setAllowCredentials(false);
-        configuration.setMaxAge(3600L);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
-
-    @Bean
-    public GlobalFilter customCorsFilter() {
-        return (exchange, chain) -> {
-            exchange.getResponse().getHeaders().set("Access-Control-Allow-Origin", "*");
-            exchange.getResponse().getHeaders().set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
-            exchange.getResponse().getHeaders().set("Access-Control-Allow-Headers", "Content-Type");
-            exchange.getResponse().getHeaders().set("Access-Control-Max-Age", "3600");
-            exchange.getResponse().getHeaders().set("Access-Control-Allow-Credentials", "false");
-            return chain.filter(exchange);
-        };
-    }
+  
 }
